c8db73b1a6f492adf00b106aa8fe836c1e10377a
kluchevsskay <Alino4ka-Vasilisa@yandex.ru>
2020-04-22 19:57:10 +0300

    помилуйте, эти боты не работают, хотя должны

--- chat_bot.py
+++ chat_bot.py
@@ -1,58 +1,28 @@
 import vk_api
 from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
-from vk_api.longpoll import VkLongPoll, VkEventType
-from vk_api.keyboard import VkKeyboard, VkKeyboardColor
+import random
 
 
 def main():
-    # API-ключ созданный ранее
-    token = ""
+    # API-ключ
+    TOKEN = "114c7052434868862e6a2a38c9b643781bc1aa968fab18720423548aa9968993d6dd72d28e6ede0d64c8e"
 
-    # Авторизуемся как сообщество
-    vk = vk_api.VkApi(token=token)
-    keyboard = VkKeyboard(one_time=True)
-    keyboard.add_button('ноу', color=VkKeyboardColor.DEFAULT)
-    keyboard.add_button('йес', color=VkKeyboardColor.POSITIVE)
-    keyboard.add_line()
+    vk_session = vk_api.VkApi(
+        token=TOKEN)
 
-    # Переход на вторую строку
-    keyboard.add_location_button()
-    keyboard.add_line()
-    keyboard.add_vkpay_button(hash="action=transfer-to-group&group_id=")
-    keyboard.add_line()
-    keyboard.add_vkapps_button(app_id=6979558,
-                               owner_id=-181108510,
-                               label="Отправить клавиатуру",
-                               hash="sendKeyboard")
+    longpoll = VkBotLongPoll(vk_session, 'ppuubblliiccc')
 
-    def write_msg(user_id, message):
-        """ ответ пользователю"""
-
-        vk.method('messages.send', {'user_id': user_id, 'message': message, 'keybord': keyboard.get_keyboard()})
-
-    # Работа с сообщениями
-    longpoll = VkLongPoll(vk)
-
-    # Основной цикл
     for event in longpoll.listen():
 
-        # Если пришло новое сообщение
-        if event.type == VkEventType.MESSAGE_NEW:
-
-            # Если оно имеет метку для меня( то есть бота)
-            if event.to_me:
-
-                # Сообщение от пользователя
-                request = event.text
-
-                # Каменная логика ответа
-                if request.lower() == "привет":
-                    write_msg(event.user_id, "Салам-пополам, дружище. Не хочешь ли сыграть в крестики-нолики, ммм",
-                              keyboard)
-                elif request.lower() == "пока":
-                    write_msg(event.user_id, "До свидания, путник :)")
-                else:
-                    write_msg(event.user_id, "Ты опечатался или хотел сказать именно это?")
+        if event.type == VkBotEventType.MESSAGE_NEW:
+            print(event)
+            print('Новое сообщение:')
+            print('Для меня от:', event.obj.message['from_id'])
+            print('Текст:', event.obj.message['text'])
+            vk = vk_session.get_api()
+            vk.messages.send(user_id=event.obj.message['from_id'],
+                             message="Спасибо, что написали нам. Мы обязательно ответим",
+                             random_id=random.randint(0, 2 ** 64))
 
 
 if __name__ == '__main__':
