ccf80eda08481560109a84fa58f8536ad5bf42e4
kluchevsskay <Alino4ka-Vasilisa@yandex.ru>
2020-04-24 19:22:38 +0300

    4ый способ создания бота, вроде работает

--- chat_bot.py
+++ chat_bot.py
@@ -1,29 +1,33 @@
 import vk_api
 from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
 import random
+import time
 
+# API-ключ
+TOKEN = "114c7052434868862e6a2a38c9b643781bc1aa968fab18720423548aa9968993d6dd72d28e6ede0d64c8e"
 
-def main():
-    # API-ключ
-    TOKEN = "114c7052434868862e6a2a38c9b643781bc1aa968fab18720423548aa9968993d6dd72d28e6ede0d64c8e"
+vk = vk_api.VkApi(
+    token=TOKEN)
 
-    vk_session = vk_api.VkApi(
-        token=TOKEN)
+vk._auth_token()
 
-    longpoll = VkBotLongPoll(vk_session, 'ppuubblliiccc')
-
-    for event in longpoll.listen():
-
-        if event.type == VkBotEventType.MESSAGE_NEW:
-            print(event)
-            print('Новое сообщение:')
-            print('Для меня от:', event.obj.message['from_id'])
-            print('Текст:', event.obj.message['text'])
-            vk = vk_session.get_api()
-            vk.messages.send(user_id=event.obj.message['from_id'],
-                             message="Спасибо, что написали нам. Мы обязательно ответим",
-                             random_id=random.randint(0, 2 ** 64))
-
-
-if __name__ == '__main__':
-    main()
+while True:
+    try:
+        messages = vk.method("messages.getConversations", {"offset": 0, "count": 20, "filter": "unanswered"})
+        if messages["count"] >= 1:
+            id = messages["items"][0]["last_message"]["from_id"]
+            body = messages["items"][0]["last_message"]["text"]
+            if body.lower() == "привет":
+                vk.method("messages.send",
+                          {"peer_id": id, "message": "салам-пополам! Желаешь сыграть в кретики-нолики?", "random_id": random.randint(1, 2147483647)})
+            elif body.lower() == "да":
+                vk.method("messages.send", {"peer_id": id, "message": "Что ж, сам напросился",
+                                            "random_id": random.randint(1, 2147483647)})
+            elif body.lower() == "нет":
+                vk.method("messages.send", {"peer_id": id, "message": "Тогда прощай, Путник",
+                                            "random_id": random.randint(1, 2147483647)})
+            else:
+                vk.method("messages.send",
+                          {"peer_id": id, "message": "Ты опе4атался или же ты именно это имел в виду?", "random_id": random.randint(1, 2147483647)})
+    except Exception as E:
+        time.sleep(1)
