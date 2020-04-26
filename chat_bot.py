624572ee6c20faa284d5bcfa0d0455be1424133b
kluchevsskay <Alino4ka-Vasilisa@yandex.ru>
2020-04-26 14:45:17 +0300

    1) добавление пояснений 2) исправление по pep-8

--- chat_bot.py
+++ chat_bot.py
@@ -15,19 +15,31 @@ while True:
     try:
         messages = vk.method("messages.getConversations", {"offset": 0, "count": 20, "filter": "unanswered"})
         if messages["count"] >= 1:
+
+            # определение id юзера
             id = messages["items"][0]["last_message"]["from_id"]
+
+            # определение текста последнего сообщения
             body = messages["items"][0]["last_message"]["text"]
+
+            # структура различных ответов на почти всевозможные сообщения от юзера
             if body.lower() == "привет":
                 vk.method("messages.send",
-                          {"peer_id": id, "message": "салам-пополам! Желаешь сыграть в кретики-нолики?", "random_id": random.randint(1, 2147483647)})
+                          {"peer_id": id, "message": "салам-пополам! Желаешь сыграть в кретики-нолики?",
+                           "random_id": random.randint(1, 2147483647)})
+
             elif body.lower() == "да":
-                vk.method("messages.send", {"peer_id": id, "message": "Что ж, сам напросился",
+                vk.method("messages.send", {"peer_id": id, "message": "что ж, сам напросился",
                                             "random_id": random.randint(1, 2147483647)})
+
             elif body.lower() == "нет":
-                vk.method("messages.send", {"peer_id": id, "message": "Тогда прощай, Путник",
+                vk.method("messages.send", {"peer_id": id, "message": "тогда прощай, Путник",
                                             "random_id": random.randint(1, 2147483647)})
+
+            # условие получение не предусмотренного мною ответа
             else:
                 vk.method("messages.send",
-                          {"peer_id": id, "message": "Ты опе4атался или же ты именно это имел в виду?", "random_id": random.randint(1, 2147483647)})
+                          {"peer_id": id, "message": "Ты опе4атался или же ты именно это имел в виду?",
+                           "random_id": random.randint(1, 2147483647)})
     except Exception as E:
         time.sleep(1)
